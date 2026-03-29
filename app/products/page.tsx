3a11eca7065e264ccc958d90606e8787c405f2e3
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-03-29 19:41:16 +0545

    learning link component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,26 @@
+import Link from "next/link";
+
 export default function Products() {
+  const productId = 100;
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>Products</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
-      <h2>Product 4</h2>
+      <h2>
+        <Link href="/products/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="/products/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="/products/3">Product 3</Link>
+      </h2>
+      <h2>
+        <Link href="/products/4">Product 4</Link>
+      </h2>
+      <h2>
+        <Link href={`/products/${productId}`} replace>Product {productId}</Link>
+      </h2>
     </>
   );
 }
