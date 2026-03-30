9efe6877f3b77a2a2fe0e79b13621bf75615bc1c
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-03-30 13:52:44 +0545

    learning templates

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <header style={{ backgroundColor: "ghostwhite", padding: "1rem" }}>
+    <footer style={{ backgroundColor: "ghostwhite", padding: "1rem" }}>
       <p>Footer</p>
-    </header>
+    </footer>
   );
 }
