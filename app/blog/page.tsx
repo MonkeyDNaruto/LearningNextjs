d1fce10089f7db4186a07f1f3ec1a6b8c0384cdc
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-03-30 14:13:06 +0545

    learning loading page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,8 @@
-export default function Blog() {
-  return <h1>My Blog</h1>
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("intentional delay");
+    }, 2000);
+  });
+  return <h1>My Blog</h1>;
 }
