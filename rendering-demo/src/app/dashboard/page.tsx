01e403998513ca459c222b0dc1f06a41ab10e450
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-04-04 13:12:46 +0545

    learning server component and client component

---
+++ rendering-demo/src/app/dashboard/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Dashboard() {
+  console.log("Dashboard component")
+  const [name, setName] = useState("");
+
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <p>Hello, {name}</p>
+    </div>
+  );
+}
