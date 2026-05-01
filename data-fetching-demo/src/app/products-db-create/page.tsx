ffe5d34d577d2bae3a25d49e22489db8ca2360f2
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-05-01 10:21:30 +0545

    learning Pending State with useFormStatus

--- data-fetching-demo/src/app/products-db-create/page.tsx
+++ data-fetching-demo/src/app/products-db-create/page.tsx
@@ -1,4 +1,5 @@
 import { addProduct } from "@/src/components/prisma-db";
+import { Submit } from "@/src/components/submit";
 import { redirect } from "next/navigation";
 
 export default function AddProductPage() {
@@ -16,35 +17,33 @@ export default function AddProductPage() {
 
   return (
     <form action={createProduct} className="p-4 space-y-4 max-w-96">
-      <label className="text-white">
-        Title
-        <input
-          type="text"
-          className="block w-full p-2 text-black border rounded"
-          name="title"
-        />
-      </label>
-      <label className="text-white">
-        Price
-        <input
-          type="number"
-          className="block w-full p-2 text-black border rounded"
-          name="price"
-        />
-      </label>
-      <label className="text-white">
-        Description
-        <textarea
-          className="block w-full p-2 text-black border rounded"
-          name="description"
-        />
-      </label>
-      <button
-        type="submit"
-        className="block w-full p-2 text-white bg-blue-500 rounded disabled:bg-gray-500"
-      >
-        Add Product
-      </button>
-    </form>
+    <label className="text-white">
+      Title
+      <input
+        type="text"
+        className="block w-full p-2 bg-white text-black border rounded"
+        name="title"
+      />
+    </label>
+  
+    <label className="text-white">
+      Price
+      <input
+        type="number"
+        className="block w-full p-2 bg-white text-black border rounded"
+        name="price"
+      />
+    </label>
+  
+    <label className="text-white">
+      Description
+      <textarea
+        className="block w-full p-2 bg-white text-black border rounded"
+        name="description"
+      />
+    </label>
+  
+    <Submit />
+  </form>
   );
 }
