9ce34bab9d87db297b5b64311c60d1528f5c3799
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-05-01 10:07:01 +0545

    learning server action

---
+++ data-fetching-demo/src/app/product-db/page.tsx
@@ -0,0 +1,31 @@
+import { getProducts } from "@/src/components/prisma-db";
+
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string | null;
+};
+
+export default async function ProductsPrismaDBPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const products: Product[] = await getProducts();
+
+  return (
+    <ul className="space-y-4 p-4">
+      {products.map((product) => (
+        <li
+          key={product.id}
+          className="p-4 bg-white shadow-md text-grey-700 rounded-lg"
+        >
+          <h2 className="text-xl font-semibold">{product.title}</h2>
+          <p>{product.description}</p>
+          <p className="text-lg font-medium">${product.price}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
