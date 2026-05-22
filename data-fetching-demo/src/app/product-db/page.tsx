5910a75b5ec6fe696171d07bacd6adf49e44af8d
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-05-22 11:14:01 +0545

    Optimistic update

--- data-fetching-demo/src/app/product-db/page.tsx
+++ data-fetching-demo/src/app/product-db/page.tsx
@@ -1,6 +1,5 @@
-import removeProduct from "@/src/actions/products";
 import { getProducts } from "@/src/components/prisma-db";
-import Link from "next/link";
+import { ProductDetail } from "./product-detail";
 
 export type Product = {
   id: number;
@@ -14,31 +13,8 @@ export default async function ProductsPrismaDBPage({
 }: {
   searchParams: Promise<{ query?: string }>;
 }) {
-  const products: Product[] = await getProducts();
+  const { query } = await searchParams;
+  const products: Product[] = await getProducts(query);
 
-  return (
-    <ul className="space-y-4 p-4">
-      {products.map((product) => (
-        <li
-          key={product.id}
-          className="p-4 bg-white text-grey-700 rounded-lg"
-        >
-          <h2 className="text-xl font-semibold">
-            <Link href={`/product-db/${product.id}`}>{product.title}</Link>
-          </h2>
-          <p>{product.description}</p>
-          <p className="text-lg font-medium">${product.price}</p>
-          <form action={removeProduct.bind(null, product.id)}>
-          <button
-              type="submit"
-              className="px-4 py-2 mt-4 text-white bg-red-500 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-600"
-            >
-              Delete
-            </button>
-          </form>
- 
-        </li>
-      ))}
-    </ul>
-  );
+  return <ProductDetail products={products} />;
 }
